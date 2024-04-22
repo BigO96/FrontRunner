743b88504bd01968717d3b18589783197f17f4fa
Oscar Epp <oepp@uoregon.edu>
2024-04-22 10:48:17 -0700

    Initial Commit

---
+++ FrontRunner/FrontRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontRunnerApp.swift
+//  FrontRunner
+//
+//  Created by Oscar Epp on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FrontRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
